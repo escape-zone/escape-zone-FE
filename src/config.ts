a0926421259787d9244fc775d18813e6801fac31
az0325 <jy960325@gmail.com>
2024-02-03 18:56:26 +0900

    delpoy test

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
-const server = '35.216.75.221:443';
-const localServer = '192.168.0.30:8080';
+const server = 'http://35.216.75.221:8080';
+const serverSecurity = 'https://35.216.75.221:443';
+const localServer = 'http://192.168.0.30:8080';
 
 const config = {
 	basename: '/',
 	defaultPath: '/',
 	fontFamily: `'Roboto', sans-serif`,
 	borderRadius: 12,
-	apiUrl: `https://${server}`,
+	apiUrl: server,
 	socketServer: `wss://${server}`
 };
 
